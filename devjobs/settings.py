4a8475687393393c963ba9980b5ed6ea95243d03
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-14 12:20:18 -0800

    test outputting env vars

--- devjobs/settings.py
+++ devjobs/settings.py
@@ -14,9 +14,16 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+print(env('GMAPS_API_KEY'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
