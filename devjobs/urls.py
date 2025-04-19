d225e321bbf3e0296707c86cf2f522babdcc30cb
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2025-04-19 12:02:57 -0700

    use base URL for API

--- devjobs/urls.py
+++ devjobs/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
 ]
